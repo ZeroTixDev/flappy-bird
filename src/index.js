49c0f5e41d41c0ed881010d4a7ec248257e53eb4
ZeroTix <weddnremy@gmail.com>
2020-12-28 17:42:53 -0500

    added some epic music

--- src/index.js
+++ src/index.js
@@ -3,29 +3,47 @@
 require('./style.css');
 const Game = require('./game');
 
+const music = document.createElement('audio');
+music.src = require('./assets/music/song.mp3').default;
+music.setAttribute('preload', 'auto');
+music.setAttribute('controls', 'none');
+music.style.display = 'none';
+music.loop = true;
+music.volume = 0.5;
+document.body.appendChild(music);
+
+const background = document.createElement('audio');
+background.src = require('./assets/music/background.mp3').default;
+background.setAttribute('preload', 'auto');
+background.setAttribute('controls', 'none');
+background.style.display = 'none';
+background.loop = true;
+background.volume = 1;
+document.body.appendChild(background);
+
 const logo = new Image();
-logo.src = require('./images/logo.png').default;
+logo.src = require('./assets/images/logo.png').default;
 logo.width = 600;
 logo.height = 160;
 document.querySelector('.container').appendChild(logo);
 
 const easyButton = document.querySelector('.easy-button');
 const easyImage = new Image();
-easyImage.src = require('./images/easy.png').default;
+easyImage.src = require('./assets/images/easy.png').default;
 easyImage.width = 200;
 easyImage.height = 200;
 easyButton.appendChild(easyImage);
 
 const hardButton = document.querySelector('.hard-button');
 const hardImage = new Image();
-hardImage.src = require('./images/hard.png').default;
+hardImage.src = require('./assets/images/hard.png').default;
 hardImage.width = 200;
 hardImage.height = 200;
 hardButton.appendChild(hardImage);
 
 const epicButton = document.querySelector('.epic-button');
 const epicImage = new Image();
-epicImage.src = require('./images/epic.png').default;
+epicImage.src = require('./assets/images/epic.png').default;
 epicImage.width = 200;
 epicImage.height = 200;
 epicButton.appendChild(epicImage);
@@ -48,6 +66,11 @@ function startGame(mode) {
    game.start();
    window.addEventListener('resize', game.resize);
    document.querySelector('.container').style.display = 'none';
+   if (mode === 'epic') {
+      music.play();
+   } else {
+      background.play();
+   }
 }
 /*
 const game = new Game({ updateRate: 60 });
